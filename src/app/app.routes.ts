084dfb7e8e8fa2e648534e7bb75736de28deb070
Aguu21 <agustinpineda04@gmail.com>
2025-04-16 08:45:48 -0300

    Solucion error con redireccion de repositorio

---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+import { BienvenidaComponent } from './bienvenida/bienvenida.component';
+import { ErrorComponent } from './error/error.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+
+export const routes: Routes = [
+
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'bienvenida', component: BienvenidaComponent },
+  { path: 'error', component: ErrorComponent },
+
+  { path: '**', redirectTo: '/error' }
+];
---
+++ src/app/registro/registro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-registro',
+  imports: [RouterLink],
+  standalone: true,
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent {}
